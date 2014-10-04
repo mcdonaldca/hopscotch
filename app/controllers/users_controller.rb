cc70d16067d1d5efe240be05168af2e7b72ec2fe
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-10-04 19:25:05 -0400

    Allow you to add bactrack_id in beginning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation, :first, :last, :phone)
+    user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation, :first, :last, :phone, :bactrack_id)
     @user = User.new user_params
     if (@user.save)
       session[:user_id] = @user.id
