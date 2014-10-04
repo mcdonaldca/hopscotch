766ed5d26eab69dbf53d2b69c541b12206080667
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-10-04 19:05:12 -0400

    LOOK HOW PRETTY IT IS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation, :first, :last, :phone)
     @user = User.new user_params
     if (@user.save)
+      session[:user_id] = @user.id
       redirect_to bac_url
     else
       render action: 'new'
