f3b01bc27b2dd3c956a8c3c6e5c1c4466da140aa
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2014-10-04 16:45:00 -0400

    Let's get this party started #HackMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hopscotch::Application.config.secret_key_base = '88f6167a69f4a0683fc05cc71efa42d62e99d4ec39177abd3893ba8d20c7f62cd89c238fdd11b7f2b1dbbcd25dcba89b5f57ba9585021511a6bee33db8ac8040'
