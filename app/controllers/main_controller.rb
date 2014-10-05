23fb0706786ba103296e9f0a4a96e7bb14e38695
Erin Hoffman <hoffm386@msu.edu>
2014-10-04 20:07:52 -0400

    Starting to Add Call to BACtrack Database

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,6 +5,13 @@ class MainController < ApplicationController
 	def bac
 	end
 
+	def getbac
+		# hard-coded latitude and longitude for now
+		response = Net::HTTP.get_response(URI("http://mobile.bactrack.com/v1/readings/?lat_center=42.358675&lng_center=-71.096509&radius_meters=1000&format=json"))
+		objects = JSON.parse(response)["objects"]
+		
+	end
+
 	def choose
 	end
 end
